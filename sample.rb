f69fff6563f64e8b5397a597584106e61f8d72e1
mygithubangebenie <angebenie23@gmail.com>
2019-07-23 11:32:11 +0200

    rubytask correction again

--- sample.rb
+++ sample.rb
@@ -1,29 +1,23 @@
 class Player
-    attr_accessor :hand
-    def initialize(hand)
-      @hand=hand
-    end
-    def hand
-      return @hand
+    
+	def hand(hand)
+		return hand
     end
 end
 
 class Enemy
-  attr_accessor :hand
-  def initialize(hand)
-    @hand=hand
-  end
-  def hand
-    return @hand
-  end
+
+	def hand(hand)
+      return hand
+    end
+
 end
+puts "Type the Number from zero to two:"
+	 puts "0: Goo"
+	 puts "1: Choki"
+	 puts "2: Par"
 	 
 	 class Janken
-		attr_accessor :player_hand,:enemy_hand
-		def initialize(player_hand,enemy_hand)
-			@player_hand=player_hand
-			@enemy_hand=enemy_hand
-		end
 		def pon(player_hand, enemy_hand)
 			result=((player_hand-enemy_hand+3)%3)
 			if(result==2)
@@ -36,27 +30,37 @@ end
 
 		end
 	 end
-
+	 
 while(true) do
-    puts "If you want to  insert any number"
-    
+
 	str=gets.to_i
 	if(str==0)
 		break
 	end
-puts "Enter the number from zero up two:"
-puts "0: Goo"
-puts "1: Choki"
-puts "2: Par"
+
 number=gets.to_i
 if(number<0 || number>3)
-	puts "out of boundary"
+	puts "It's a Wrong number"
 else
-	player = Player.new(number)
+	player = Player.new()
+	player_hand = player.hand(number)
 	arr=[0,1,2]
 	randomly=arr[rand(arr.count)];
-	enemy = Enemy.new(randomly)
-	janken = Janken.new(player,enemy)
-	puts " it is a #{janken.pon(player.hand, enemy.hand)}"
+
+	enemy = Enemy.new()
+	enemy_hand=enemy.hand(randomly)
+	janken = Janken.new()
+	result = janken.pon(player_hand,enemy_hand)
+	if(result=="Win")
+		puts "The hand of the other party is throb. You are the winner."
+		break
+	elsif(result=="Loss")
+       puts "you are loss"
+		break
+	else
+		puts "nothing" 
+
+	end
+	
 end
 end
