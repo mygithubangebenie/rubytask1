c968609096a3c5b029de70658d29fe923ce17f89
mygithubangebenie <angebenie23@gmail.com>
2019-07-23 15:38:53 +0200

    rubytask1  correction of doublenumber

--- sample.rb
+++ sample.rb
@@ -6,7 +6,6 @@ class Player
 end
 
 class Enemy
-
 	def hand(hand)
       return hand
     end
@@ -33,32 +32,27 @@ puts "Type the Number from zero to two:"
 	 
 while(true) do
 
-	str=gets.to_i
-	if(str==0)
-		break
-	end
-
 number=gets.to_i
 if(number<0 || number>3)
 	puts "It's a Wrong number"
 else
-	player = Player.new()
+	player = Player.new
 	player_hand = player.hand(number)
 	arr=[0,1,2]
 	randomly=arr[rand(arr.count)];
 
-	enemy = Enemy.new()
+	enemy = Enemy.new
 	enemy_hand=enemy.hand(randomly)
-	janken = Janken.new()
+	janken = Janken.new
 	result = janken.pon(player_hand,enemy_hand)
 	if(result=="Win")
-		puts "The hand of the other party is throb. You are the winner."
+		puts "You are the winner."
 		break
 	elsif(result=="Loss")
        puts "you are loss"
 		break
 	else
-		puts "nothing" 
+		puts "it is draw" 
 
 	end
 	
